89511eac4fa29a64453291b3115ded47549e7bd4
metin <metinmertkirdim@hotmail.com>
2022-04-02 11:38:18 -0400

    Updated TC001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -2,6 +2,7 @@ package test_cases;
 
 public class TC001 {
     public static void main(String[] args) {
-        // testing some thing
+        // testing something
+        // more testing
     }
 }
