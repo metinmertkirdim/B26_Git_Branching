e7cd7fd866b7cc7ed1da2a56df329eaa9cfeece7
metin <metinmertkirdim@hotmail.com>
2022-04-02 14:40:31 -0400

    Updated TC002

--- src/test_cases/TC002.java
+++ src/test_cases/TC002.java
@@ -4,5 +4,7 @@ public class TC002 {
     public static void main(String[] args) {
         // Test Case 2
         // writing smoke tests...
+        // creating a report
+        // screenshot in failure
     }
 }
